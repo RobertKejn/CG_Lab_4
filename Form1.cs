64f998100bfc5360baa8d34adf0bbcab3d0f88bc
Robert Kein <robertkejn@gmail.com>
2022-10-27 18:24:10 +0400

    Normal Thresholding Added

--- Form1.cs
+++ Form1.cs
@@ -8,13 +8,14 @@ namespace CG_4
         PictureBox initialPicture = new PictureBox();
         PictureBox grayPicture = new PictureBox();
         PictureBox thresPicture = new PictureBox();
+        PictureBox diagPicture = new PictureBox();
         public Form1()
         {
             this.StartPosition = FormStartPosition.CenterScreen;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.FormBorderStyle = FormBorderStyle.FixedDialog;
-            this.Size = new Size(Properties.Resources.picture_hand_compressed.Width*3+20, Properties.Resources.picture_hand_compressed.Height);
+            this.Size = new Size(Properties.Resources.picture_hand_compressed.Width*3+20, Properties.Resources.picture_hand_compressed.Height+ 10 + 256);
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.Show();
@@ -36,6 +37,12 @@ namespace CG_4
             thresPicture.Location = new Point((initialPicture.Width + 10)*2, 0);
             thresPicture.Visible = true;
             this.Controls.Add(thresPicture);
+
+            diagPicture.Size = new Size(256, 256);
+            diagPicture.Image = new Bitmap(256, 256);
+            diagPicture.Location = new Point(0, (initialPicture.Height + 10));
+            diagPicture.Visible = true;
+            this.Controls.Add(diagPicture);
         }
 
         private void initialPictureClick(object sender, MouseEventArgs args)
@@ -54,10 +61,27 @@ namespace CG_4
             grayPicture.Image = nbm;
         }
 
+        private void DrawDiag(byte[] diag)
+        {
+            Bitmap bm = new Bitmap(diag.Length, diag.Length);
+            for(int i = 0; i < bm.Width; i++)
+            {
+                for(int j = 0; j < bm.Height; j++)
+                {
+                    bm.SetPixel(i, j, Color.FromArgb(255,255,255,255));
+                }
+            }
+            for(int i = 0; i < diag.Length; i++)
+            {
+                bm.SetPixel(i, 255-diag[i], Color.FromArgb(255, 0, 0, 0));
+            }
+            diagPicture.Image = bm;
+        }
         private void imageThresholding(object sender, MouseEventArgs args)
         {
-            byte lim = 150;
-            if( (sender as PictureBox).Image != null){
+             var diag = GiagramCreation(3);
+            DrawDiag(diag);
+            if ( (sender as PictureBox).Image != null){
                 Bitmap bm = (Bitmap)((sender as PictureBox).Image);
                 Bitmap nbm = new Bitmap(bm.Width, bm.Height);
                 for(int i = 0; i < bm.Width; i++)
@@ -65,13 +89,30 @@ namespace CG_4
                     for(int j = 0; j < bm.Height; j++)
                     {
                         Color c = bm.GetPixel(i, j);
-                        byte br = (byte)(c.R * 0.3 + c.G * 0.59 + c.B * 0.11);
-                        if (br > lim) nbm.SetPixel(i, j, Color.FromArgb(255, 255, 255, 255));
+                        byte br = (byte)((c.R + c.G + c.B)/3);
+                        if (br > diag[br]) nbm.SetPixel(i, j, Color.FromArgb(255, 255, 255, 255));
                         else nbm.SetPixel(i, j, Color.FromArgb(255, 0, 0, 0));
                     }
                 }
                 thresPicture.Image = nbm;
             }
+        }
+
+        private byte[] GiagramCreation(int n)
+        {
+            byte[] diag = new byte[256];
+            int dx = 256 / n;
+            //int dy = n;
+            int cx = 0;
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = cx; j < (i+1)*dx; j++)
+                {
+                    diag[j] = (byte)(j*n);
+                }
+                cx += dx;
+            }
+            return diag;
         }
         
 
